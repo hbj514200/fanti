290558627a2500227cafaab9a66557774b7df0d3
hbj514200 <1336892373@qq.com>
2016-09-11 20:33:56 +0800

    youmi OK

--- app/src/main/java/com/qq/qzone/a1336892373/fantai/MainActivity.java
+++ app/src/main/java/com/qq/qzone/a1336892373/fantai/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +15,7 @@ import android.widget.Toast;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import net.youmi.android.normal.spot.SpotManager;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button jButton;
     private Button fButton;
@@ -20,6 +23,14 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private ImageView yuanbutton;
     private int yuanflag = 1;
     private FirebaseAnalytics mFirebaseAnalytics;
+    Handler myhandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            SpotManager.getInstance(MainActivity.this).setSpotOrientation(SpotManager.ORIENTATION_PORTRAIT);
+            SpotManager.getInstance(MainActivity.this).showSpotAds(MainActivity.this);
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +40,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         jButton = (Button) findViewById(R.id.jbutton);
         fButton = (Button) findViewById(R.id.fbutton);
         editText = (EditText) findViewById(R.id.edittext);
-        yuanbutton=  (ImageView) findViewById(R.id.yuanbutton);
+        yuanbutton = (ImageView) findViewById(R.id.yuanbutton);
 
         jButton.setOnClickListener(this);
         fButton.setOnClickListener(this);
@@ -39,10 +50,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try { Thread.sleep(45000); } catch (Exception e) { }
-                SpotManager.getInstance(MainActivity.this).setSpotOrientation(SpotManager.ORIENTATION_PORTRAIT);
-                SpotManager.getInstance(MainActivity.this).setAnimationType(SpotManager.ANIM_SIMPLE);
-                SpotManager.getInstance(MainActivity.this).showSpotAds(MainActivity.this);
+                try { Thread.sleep(40000); } catch (Exception e) { }
+                myhandler.sendMessage( new Message() );
             }
         }).start();
 
@@ -50,21 +59,20 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.jbutton :
+        switch (v.getId()) {
+            case R.id.jbutton:
                 editText.setText(tools.tofanti(editText.getText().toString()));
                 break;
-            case R.id.fbutton :
+            case R.id.fbutton:
                 editText.setText(tools.tojianti(editText.getText().toString()));
                 break;
-            case R.id.yuanbutton :
-                if (yuanflag%2==1){
+            case R.id.yuanbutton:
+                if (yuanflag % 2 == 1) {
                     yuanbutton.setImageResource(R.drawable.yuanbutton2);
                     ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                     editText.setText(cm.getText());
                     cm.setText("");
-                }
-                else{
+                } else {
                     yuanbutton.setImageResource(R.drawable.yuanbutton);
                     Toast.makeText(MainActivity.this, getResources().getString(R.string.yijin), Toast.LENGTH_SHORT).show();
                     ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
